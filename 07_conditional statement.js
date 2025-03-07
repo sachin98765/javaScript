4f87dd88e5dff091bc0e36efd804bbfc40b6c45e
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-03-08 01:26:45 +0530

    operator conditional statement loops added

---
+++ 07_conditional statement.js
@@ -0,0 +1,40 @@
+// conditional statements
+
+/* 
+    if
+    if---else
+    if-- else if-- else
+    nested if
+
+*/
+
+// let num1 = 3
+//     if (num1 == 3) {
+//     console.log("The number is 3")
+//     } else if (num1 == 7) {
+//     console.log("The number is 7")
+//     } else {
+//     console.log("The number is not 3")
+//     }
+
+//switch case
+//  let num1 = 3
+//     switch (num1) {
+//         case 1:
+//             console.log("The number is 1")
+//             break;
+//         case 3:
+//             console.log("The number is 3")
+//             break;
+//         case 7:
+//             console.log("The number is 7")
+//             break;
+//         default:
+//             console.log("The number is not 3")
+//             break;
+//     }
+
+//ternary operator
+let num1 = 3
+let result = num1 == 3 ? "The number is 3" : "The number is not 3"
+console.log(result)
---
+++ 08_loops.js
@@ -0,0 +1,54 @@
+/*
+   for
+   while
+   do-- while
+    for-- in
+    for-- of
+    forEach
+
+
+*/
+
+// #1 for loop
+
+// for(let i=0; i<5; i++){
+//     console.log(i);
+// }
+
+// #2 while loop
+// let i = 0
+// while (i < 5) {
+//   console.log(i)
+//   i++
+// }
+
+// #3 do while loop
+// let i = 0
+// do {
+//   console.log(i)
+//   i++
+// } while (i < 5)
+
+// #4 for in loop
+// let obj = {
+//   name: "John", //key:value
+//   age: 30,
+//   city: "New York", //key:value
+// }
+// for (let key in obj) {
+//   //key is a variable
+//   console.log(key)
+//   console.log(obj[key])
+// }
+
+// #5 for of loop
+// let arr = [3, 5, 7]
+// for (let value of arr) {
+//   console.log(value)
+// }
+
+// #6 forEach loop
+// let arr = [3, 5, 7]
+// arr.forEach(function (value, index) {
+//   console.log(value, index)
+// })
