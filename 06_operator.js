4394676bce41810e20b232988b42873fd872f628
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-03-08 01:04:52 +0530

    color-changer operator

---
+++ 06_operator.js
@@ -0,0 +1,26 @@
+//Arithmetic Operators
+// console.log(3+4+5)
+// console.log(3-2)
+// console.log(3*3)
+// console.log(3/2)
+// console.log(3%2)
+// console.log(3**2)
+
+//increment and
+let x = 10
+// console.log(x++)
+// console.log(x)
+console.log(++x)
+console.log(x)
+
+//decrement operators
+// let y=3
+// console.log(y--)
+// console.log(y)
+// console.log(--y)
+// console.log(y)
+
+//Assignment Operators
+// let m = 5;
+// m += 4;
+// console.log(x)
