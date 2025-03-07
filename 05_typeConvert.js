e0324f334c81225d3050050961116ad70f698749
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-03-07 23:24:59 +0530

    add all files

---
+++ 05_typeConvert.js
@@ -0,0 +1,25 @@
+//value to string
+
+//1. number to String
+//2. boolean to String
+
+/*
+let num1 = 45
+console.log(typeof String(num1), num1)
+console.log(typeof num1.toString(), num1)
+
+*/
+
+let bool = true
+console.log(typeof String(bool))
+
+
+//value to number 
+
+//1. string to number
+let myName ="Kishan"
+console.log(typeof Number(myName), myName)
+
+//2. boolean to number
+let bool1 = true
+console.log(typeof Number(bool1), bool1)
