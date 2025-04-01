3ab4f2524038a6d5354abf8264a45ebc85a3da69
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-04-01 21:00:37 +0530

    rock paper sessior

---
+++ rock-paper-scissor/script.js
@@ -0,0 +1,46 @@
+const btnEle=document.querySelectorAll("button")
+
+const resultEle = document.getElementById("result")
+const userScoreEle = document.getElementById("user-score")
+const compScoreEle = document.getElementById("comp-score")
+
+let userScore =0;
+let compScore=0;
+
+btnEle.forEach((btn) => {
+    btn.addEventListener("click",()=>{
+       /* console.log("you clicked on",btn.id)
+        console.log("Computer Choice",compChoice()) */
+       const result =  playRound(btn.id,compChoice())
+       /*console.log(result) */
+       resultEle.textContent = result
+
+    })
+});
+
+function compChoice(){
+    const choices =["rock","paper","scissor"]
+    const randomChoice= Math.floor(Math.random()*choices.length)
+
+    return choices[randomChoice]
+
+}
+
+function playRound(userSelection, compSelection){
+    if(userSelection === compSelection){
+        return "Its's a tie!"
+    }else if(
+        (userSelection === "rock" && compSelection === "scissor") || 
+        (userSelection === "paper" && compSelection === "rock") ||
+        (userSelection === "scissor" && compSelection === "paper") 
+    ){
+        userScore++
+        userScoreEle.textContent = userScore
+        return `You won! ${userSelection} beats ${compSelection}`
+    }else{
+        compScore++
+        compScoreEle.textContent = compScore
+        return `You lose! ${compSelection} beats ${userSelection} `
+    }
+
+}
